9eff1f27ac674db8ad97068d9a0baf7c8eb66763
glorycode02 <glorycode02@gmail.com>
2025-01-02 18:08:49 +0200

    notifytune

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: must_be_immutable, prefer_const_constructors
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
